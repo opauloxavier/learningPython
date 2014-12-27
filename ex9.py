45cc594cda3ff5c04e8e671e0a4f135583810882
unuzual <contato.pauloxavier@gmail.com>
2014-12-27 15:34:28 -0200

    Update

---
+++ ex9.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, first, second, third = argv
+
+total=raw_input("bet on the total:")
+
+result= int(total) == (int(first)+int(second)+int(third))
+
+print "The script is called:", script
+print "Your sum is:", int(first)+int(second)+int(third)
+
+print "Your bet was:", result
